999f24d98554a161add00767b09c49b1fe195634
ipsluizeduardo <ipsluizeduardo@gmail.com>
2023-05-02 09:14:55 -0400

    aula2

--- 5-objetos/aula.js
+++ 5-objetos/aula.js
@@ -1,4 +1,22 @@
-const vitor = {
+/* Reatribuição de propriedades
+pessoa.["nome"] = "teste"; (Dinamico)
+pessoa.nome = "teste";(Diretamente)
+*/
+
+const pessoa = {
     nome: "Vitor",
-    idade: 25
+    idade: 25,
+
+    descrever : function () {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    }
 }
+
+// pessoa.altura = 1.69;
+// delete pessoa.altura;
+//pessoa.descrever();
+
+const atributo = "idade";
+
+console.log(pessoa[atributo])
+console.log(pessoa["nome"])
